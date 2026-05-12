1e35e93eb5a3c1e7178d6b3cc29f96f3ad274f13
Saurabh <saurabhsharma090903@gmail.com>
2026-05-12 23:54:18 +0530

    Setting up the basic server connection file , DB connection files and Enviornment setup

--- src/config/connection.DB.js
+++ src/config/connection.DB.js
@@ -1,1 +1,10 @@
 import mongoose from "mongoose";
+
+export default async function ConnectDB() {
+  try {
+    await mongoose.connect("connection string");
+    console.log("Connected , Successfully with the DataBase");
+  } catch (error) {
+    console.log("Failed to connect with the Database!");
+  }
+}
--- src/config/enviornment.config.js
+++ src/config/enviornment.config.js
@@ -1,1 +1,7 @@
 import dotenv from "dotenv";
+
+//Here we have created this file Inside the src/config folder so we have to provide it the path through which it can access the .env
+//Generally both should be at the same path means at the root.
+dotenv.config();
+
+console.log(process.env.PORT);//currently it will be undefined.
