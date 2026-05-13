a9492418ae2eeb5784f93e6614d374f56eb8431e
Saurabh <saurabhsharma090903@gmail.com>
2026-05-13 23:54:08 +0530

    we have setup the path of the environment variables and are ready inject them

--- src/config/enviornment.config.js
+++ src/config/enviornment.config.js
@@ -1,7 +1,11 @@
 import dotenv from "dotenv";
-
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 //Here we have created this file Inside the src/config folder so we have to provide it the path through which it can access the .env
 //Generally both should be at the same path means at the root.
-dotenv.config();
 
-console.log(process.env.PORT);//currently it will be undefined.
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+//through these path resolution we resolved the whole paths and finally our .env variable are ready to be injected.
+
+dotenv.config({ path: path.resolve(__dirname, "../../.env") });
